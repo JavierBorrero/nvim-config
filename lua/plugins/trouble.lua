3c0fbeffcdce0c0c92abd9e6b45507e6233d8e64
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-04-17 13:20:16 +0200

    add trouble.nvim

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,19 @@
+return {
+	"folke/trouble.nvim",
+	opts = {},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xL",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
+}
